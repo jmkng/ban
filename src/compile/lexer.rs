51c9c135a48a9f356047065ed49812b46db54ce2
Jon Koenig <jkoenig@outlook.com>
2023-06-26 08:12:12 -0600

    refactor lex_default to support whitespace trim character, add tests

--- src/compile/lexer.rs
+++ src/compile/lexer.rs
@@ -12,6 +12,9 @@ pub struct Lexer<'source> {
     state: State,
     /// Position within source.
     cursor: usize,
+    /// When true, the following Token pulled in State::Default state
+    /// will be left trimmed.
+    left_trim: bool,
     /// Temporary storage for a "next" token that will be read
     /// on the following call to [.next()]
     buffer: Option<Region<Token>>,
@@ -23,6 +26,7 @@ impl<'source> Lexer<'source> {
             finder: Search::new(SyntaxBuilder::new().build()),
             state: State::Default,
             source,
+            left_trim: false,
             cursor: 0,
             buffer: None,
         }
@@ -50,13 +54,31 @@ impl<'source> Lexer<'source> {
     fn lex_default(&mut self, from: usize) -> Option<Region<Token>> {
         // Find the next opening expression/block, return everything up to
         // that point as Token::Raw.
+
+        // A closure which trims the bounds of the given indices and returns them
+        // as a Region, with a Token::Raw embedded in the data property.
+        //
+        // The only Token type which should ever be trimmed is Token::Raw.
+        let mut trim_region = |mut region_begin, mut region_end, right_trim| {
+            if right_trim {
+                region_end = self.source[..region_end].trim_end().len();
+            }
+            if self.left_trim {
+                self.left_trim = false;
+                let s = &self.source[region_begin..region_end];
+                region_begin = s.len() - s.trim_start().len()
+            }
+
+            Some(Region::new(Token::Raw, region_begin..region_end))
+        };
+
         match self.finder.find_at(self.source, from) {
-            Some((id, begin, end)) => {
+            Some((id, marker_begin, marker_end)) => {
                 // Found a marker, so return everything up to that marker first as
                 // Token::Raw.
                 //
                 // The marker can be stored in the buffer, it will be read next.
-                let (token, trim) = Token::from_usize_trim(id);
+                let (token, is_trimmed) = Token::from_usize_trim(id);
 
                 match &token {
                     Token::BeginExpression => self.state = State::Expression,
@@ -65,30 +87,27 @@ impl<'source> Lexer<'source> {
                     _ => panic!("unexpected token in top-level scope"),
                 }
 
-                let mut lex = |token: Token, range: Range<usize>| {
-                    self.cursor = range.end;
-
-                    Some(Region::new(token, range))
-                };
-
-                if from == begin {
+                if from == marker_begin {
                     // No raw text is between our cursor and the marker, so we can
                     // skip storing it on the buffer and just return it right away.
-                    lex(token, begin..end)
+                    self.cursor = marker_end;
+                    Some(Region::new(token, marker_begin..marker_end))
                 } else {
                     // Store the marker in buffer.
-                    self.buffer = lex(token, begin..end);
+                    self.cursor = marker_end;
+                    self.buffer = Some(Region::new(token, marker_begin..marker_end));
+
                     // Return the Token::Raw pointing to everything up to the
                     // beginning of the marker.
-                    lex(Token::Raw, from..begin)
+                    trim_region(from, marker_begin, is_trimmed)
                 }
             }
             None => {
                 // Nothing found. Advance to the end.
-                let range = self.cursor..self.source.len();
+                let remaining = self.cursor..self.source.len();
                 self.cursor = self.source.len();
 
-                Some(Region::new(Token::Raw, range))
+                trim_region(from, remaining.end, false)
             }
         }
     }
@@ -102,16 +121,27 @@ enum State {
 
 #[cfg(test)]
 mod tests {
+    use super::Lexer;
     use crate::{compile::token::Token, types::Region};
 
-    use super::Lexer;
+    #[test]
+    fn test_lex_default_no_match() {
+        let mut lexer = Lexer::new("lorem ipsum");
+        let expect = Region::new(Token::Raw, 0..11);
+        assert_eq!(lexer.next(), Some(expect))
+    }
 
     #[test]
-    fn test_lex_default() {
-        let mut lexer = Lexer::new("test string");
-
-        let expect = Region::new(Token::Raw, 0..11);
-
+    fn test_lex_default_match_no_trim() {
+        let mut lexer = Lexer::new("lorem ipsum (( dolor ...");
+        let expect = Region::new(Token::Raw, 0..12);
         assert_eq!(lexer.next(), Some(expect))
+    }
+
+    #[test]
+    fn test_lex_default_match_trim() {
+        let source = "lorem ipsum ((- dolor ...";
+        let mut lexer = Lexer::new(source);
+        assert_eq!(lexer.next(), Some(Region::new(Token::Raw, 0..11)))
     }
 }
